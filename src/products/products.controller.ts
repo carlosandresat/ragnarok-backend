ba8c27b46f793f96eee0bd9fdad7dc49110a6b0c
Carlos Arevalo <carlosandresat@hotmail.com>
2025-01-22 23:50:30 -0500

    feat(products): add getProduct method and @Get route for a product info
    - Add some items to  products array in ProductsService.
    - Added getProduct() method to retrieve a specific product.
    - Created @Get(':id') route in ProductsController to handle getProduct().

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
@@ -8,5 +8,10 @@ export class ProductsController {
   @Get()
   getAllTasks() {
     return this.productsService.getTasks();
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') id: string) {
+    return this.productsService.getProduct(parseInt(id));
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,9 +2,36 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class ProductsService {
-  private products = [];
+  private products = [
+    {
+      id: 1,
+      name: 'Product 1',
+      description: 'This is product 1',
+      price: 100,
+    },
+    {
+      id: 2,
+      name: 'Product 2',
+      description: 'This is product 2',
+      price: 150,
+    },
+    {
+      id: 3,
+      name: 'Product 3',
+      description: 'This is product 3',
+      price: 200,
+    },
+  ];
 
   getTasks() {
     return this.products;
+  }
+
+  getProduct(id: number) {
+    const product = this.products.find((item: any) => item.id == id);
+    if (!product) {
+      throw new NotFoundException(`Product with ID ${id} not found`);
+    }
+    return product;
   }
 }
