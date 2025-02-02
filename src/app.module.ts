7ba82c72fb5d3023fefcf6cb89ba3cbebd936f97
Carlos Arevalo <carlosandresat@hotmail.com>
2025-02-02 16:37:34 -0500

    chore(database): setup SQLite database with TypeORM
    - Installed TypeORM and SQLite dependencies.
    - Configured database connection in TypeORM settings.
    - Set up initial database module for handling entities.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
 import { CartModule } from './cart/cart.module';
 import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [ProductsModule, CartModule, OrdersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    ProductsModule,
+    CartModule,
+    OrdersModule,
+  ],
 })
 export class AppModule {}
