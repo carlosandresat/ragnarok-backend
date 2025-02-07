b8c07c5ec011f0d8a173ccb6ce4952b6a9802647
Carlos Arevalo <carlosandresat@hotmail.com>
2025-02-06 22:20:45 -0500

    feat(products): Integrate CategoryService and CategoryController into ProductsModule

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,10 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
 import { Products } from './entities/products.entity';
+import { CategoryService } from './category.service';
+import { CategoryController } from './category.controller';
+import { Categories } from './entities/categories.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Products])],
-  providers: [ProductsService],
-  controllers: [ProductsController]
+  imports: [TypeOrmModule.forFeature([Products, Categories])],
+  providers: [ProductsService, CategoryService],
+  controllers: [ProductsController, CategoryController],
 })
 export class ProductsModule {}
